47df5713403177023679f131157983418c0e2b21
michael.o.lam <michael.o.lam@fa5df4a4-9e22-0410-866f-df591ab03656>
2006-12-09 05:26:03 +0000

    Added comments to the key handler generation script

--- src/demo/keyboarddemo/generate_key_handlers.sh
+++ src/demo/keyboarddemo/generate_key_handlers.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Yet another crazy sed script! :-D
+
+# This one pulls out event methods from KeyboardListener.h
+# and generates methods for this demo, storing them
+# temporarily in handlecode.cpp.
+
 sed -e 's/\tinline virtual void //' < ../../event/KeyboardListener.h |
 sed -e 's/\(\) {}//' | sed -e 's/.*/\tvoid KeyboardDemo::& { cout << "&" << endl; }/' > handlecode.cpp
 
