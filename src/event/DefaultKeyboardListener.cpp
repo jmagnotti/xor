21249694126aade0bd336c966530de50a0722e59
john.magnotti <john.magnotti@fa5df4a4-9e22-0410-866f-df591ab03656>
2006-12-27 22:54:23 +0000

    made keyhandlers protected, non Keylisteners shouldn't be calling handlekey. Changed DFKL to use the new Positionable interface of Viewer

--- src/event/DefaultKeyboardListener.cpp
+++ src/event/DefaultKeyboardListener.cpp
@@ -202,26 +202,28 @@ void DefaultKeyboardListener::handleKey(KeyEvent* ke)
 
 void DefaultKeyboardListener::handleKey_w()
 {
-	Controller::GetInstance()->getViewer()->getOrientation()->walk(0.05f);
+	Controller::GetInstance()->getViewer()->walk(0.05f);
 }
 
 void DefaultKeyboardListener::handleKey_s()
 {
-	Controller::GetInstance()->getViewer()->getOrientation()->walk(-0.05f);
+	Controller::GetInstance()->getViewer()->walk(-0.05f);
 }
 
 void DefaultKeyboardListener::handleKey_q()
 {
-	Controller::GetInstance()->getViewer()->getOrientation()->incrementRotation(2, 5.0f);
+	Controller::GetInstance()->getViewer()->incrementRotation(2, 5.0f);
 }
 
 void DefaultKeyboardListener::handleKey_e()
 {
-	Controller::GetInstance()->getViewer()->getOrientation()->incrementRotation(2, -5.0f);
+	Controller::GetInstance()->getViewer()->incrementRotation(2, -5.0f);
 }
 
 void DefaultKeyboardListener::handleKey_F()
 {
+    cout << "Toggle Full Screen" << endl;
+
 	Controller::GetInstance()->getViewer()->toggleFullScreen();
 }
 
