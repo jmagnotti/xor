26fc4c7c5d4a8ebeb356b8d0668845b414101943
john.magnotti <john.magnotti@fa5df4a4-9e22-0410-866f-df591ab03656>
2007-02-04 00:47:40 +0000

    demo of multicast code

---
+++ trunk/src/demo/multicast/sendDriver.cpp
@@ -0,0 +1,19 @@
+#include "../../xor.h"
+
+using namespace XOR;
+
+int main(int argc, char * argv[])
+{
+	MulticastSocketPool * pool = MulticastSocketPool::GetInstance();
+
+	MulticastSocket * out =
+		pool->getMulticastSocket(MulticastSocketPool::KEYBOARD_SOCKET);
+
+	string msg = "Hello";
+	out->send(msg);
+
+	pool->cleanUpAndExit();
+
+	return 0;
+}
+
