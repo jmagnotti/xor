5456cb99c058e8d8ab323201cb19537ac3d304db
john.magnotti <john.magnotti@fa5df4a4-9e22-0410-866f-df591ab03656>
2007-04-02 20:07:27 +0000

    Initial commit of project icarus, XOR event front-end

---
+++ trunk/icarus/main/IcarusDriver.java
@@ -0,0 +1,24 @@
+package main;
+
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+public class IcarusDriver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		JFrame frame = new IcarusFrame();
+		frame.setBounds(20, 20, 600, 480);
+		try {
+			UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+		} catch (Exception e) {
+		}
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+	}
+
+}
