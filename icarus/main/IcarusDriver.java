a98439da44466cb2c9a26ebb2d882e1e73ffd43d
john.magnotti <john.magnotti@fa5df4a4-9e22-0410-866f-df591ab03656>
2007-06-28 18:50:12 +0000

    Multicast in the textbox and new images

--- icarus/main/IcarusDriver.java
+++ icarus/main/IcarusDriver.java
@@ -1,26 +1,34 @@
 package main;
 
-
 import javax.swing.JFrame;
 import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class IcarusDriver {
 
 	/**
 	 * @param args
+	 * @throws UnsupportedLookAndFeelException
+	 * @throws IllegalAccessException
+	 * @throws InstantiationException
+	 * @throws ClassNotFoundException
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException,
+			InstantiationException, IllegalAccessException,
+			UnsupportedLookAndFeelException {
 		JFrame frame = new IcarusFrame();
 		frame.setBounds(20, 20, 600, 480);
-		/*
-			try {
-				UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-			} catch (Exception e) {
-			}
-		*/
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
+		UIManager.setLookAndFeel(UIManager
+				.getCrossPlatformLookAndFeelClassName());
+		/*
+		 * try {
+		 * UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName()); }
+		 * catch (Exception e) { }
+		 */
+		frame.setVisible(true);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 	}
 
 }
