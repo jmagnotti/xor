6791b73c9b79ef17d9e1df1036d296751e36c45b
john.magnotti <john.magnotti@fa5df4a4-9e22-0410-866f-df591ab03656>
2011-03-21 15:59:16 +0000

    changes for texture. PNG isn't working

--- trunk/src/texture/Texture.cpp
+++ trunk/src/texture/Texture.cpp
@@ -105,34 +105,59 @@ void Texture::loadFromFile()
 /**
  * Does the GL/SDL dirtwork for building a texture
  */
-void Texture::buildTexture(SDL_Surface * texture)
+void Texture::buildTexture(SDL_Surface * surface)
 {
-	//Done in TextureFactory
-	//glEnable(GL_TEXTURE_2D);
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	//glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+    int w, h;
+    SDL_Surface *image;
+    SDL_Rect area;
+    Uint32 saved_flags;
+    Uint8 saved_alpha;
+	
+    /* Use the surface width and height expanded to powers of 2 */
+    w = (surface->w);
+    h = (surface->h);
+	
+    image = SDL_CreateRGBSurface(SDL_SWSURFACE, w, h, 32,
+#if SDL_BYTEORDER == SDL_LIL_ENDIAN     /* OpenGL RGBA masks */
+                                 0x000000FF,
+                                 0x0000FF00, 0x00FF0000, 0xFF000000
+#else
+                                 0xFF000000,
+                                 0x00FF0000, 0x0000FF00, 0x000000FF
+#endif
+								 );
 
-	//if (SDL_MUSTLOCK(texture)) SDL_LockSurface(texture);
+    /* Save the alpha blending attributes */
+    saved_flags = surface->flags & (SDL_SRCALPHA | SDL_RLEACCELOK);
+    SDL_GetSurfaceAlphaMod(surface, &saved_alpha);
+    if ((saved_flags & SDL_SRCALPHA) == SDL_SRCALPHA) {
+        SDL_SetAlpha(surface, 0, 0);
+    }
+	
+    /* Copy the surface into the GL texture image */
+    area.x = 0;
+    area.y = 0;
+    area.w = surface->w;
+    area.h = surface->h;
+    SDL_BlitSurface(surface, &area, image, &area);
+	
+    /* Restore the alpha blending attributes */
+    if ((saved_flags & SDL_SRCALPHA) == SDL_SRCALPHA) {
+        SDL_SetAlpha(surface, saved_flags, saved_alpha);
+    }
+	
+    /* Create an OpenGL texture for the image */
     glGenTextures(1, &_id);
     glBindTexture(GL_TEXTURE_2D, _id);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D,
+                 0,
+                 GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image->pixels);
+    SDL_FreeSurface(image);     /* No longer needed */
+     _width  = surface->w;
+     _height = surface->h;
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-
-     _width  = texture->w;
-     _height = texture->h;
-
-     if (texture->format->BytesPerPixel == 4) {
-        //gluBuild2DMipmaps(GL_TEXTURE_2D, 3, _width, _height, GL_BGRA, GL_UNSIGNED_BYTE, texture->pixels);
-        glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, texture->w, texture->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, texture->pixels );
-	 }
-	 else {
-		//gluBuild2DMipmaps(GL_TEXTURE_2D, 3, _width, _height, GL_BGR, GL_UNSIGNED_BYTE, texture->pixels);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texture->w, texture->h, 0, GL_RGB, GL_UNSIGNED_BYTE, texture->pixels);
-	 }
-	 //if (SDL_MUSTLOCK(texture)) SDL_UnlockSurface(texture);
-	 //SDL_FreeSurface(texture);
 }
 
 
